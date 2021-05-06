f8ba468c25a3c81ff84aad09e8a29e861ead980e
Gokulraj R <gokulraj18921@gmail.com>
2021-05-07 01:09:34 +0530

    Added a few structs to use in task2.c

--- covid.h
+++ covid.h
@@ -63,26 +63,35 @@ typedef struct Node {
 } Node;
 
 
-/////// For Dijkstra ////////
+/////// For task2 ////////
 
-typedef struct linkedlistNode_for_SPTable_PreviousVertex {
-    ll station_no;
-    struct linkedlistNode_for_SPTable_PreviousVertex* next;
-} ll_node;
+typedef struct path_info {
+    ll danger_value;
+    ll length;
+    ll no_of_vertices_in_the_path;
+    ll* path_vertices;
+} path_info;
 
-typedef struct stSPTable {
-    int *pKnown;
-    double *pdistance_to_source;
-    ll_node** pPrevious_vertex;
-} SPTable;
+// a struct to return the danger value and the length of the removed edge
+typedef struct Set {
+    ll danger_value;
+    ll length;
+} Set;
+
+int isConnected(Graph* G);
+void GetPath (ll* previous_vertex, ll destination, path_info* path, ll* index);
+path_info* dijkstra (Graph* G, ll source, ll destination);
+top_three_routes (Graph* G, ll source, ll destination);
 
 ////////////////////
 
 Graph* create_graph(ll no_of_stations, ll no_of_roads, ll total_no_of_people);
-void insert_edge(Graph* G, ll source, ll destination, ll length);
-void insert_bidirectional_road(Graph* G, ll source, ll destination, ll length);
+void insert_edge(Graph* G, ll source, ll destination, ll length, ll danger_value);
+void insert_bidirectional_road(Graph* G, ll source, ll destination, ll length, ll danger_value);
 Person* initialize_people(ll no_of_people);
 
+Set remove_edge(Graph* G, ll source, ll destination);
+Set remove_bidirectional_road(Graph* G, ll source, ll destination);
 
 
 
