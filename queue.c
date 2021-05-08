b65c59ac7c1d56e844031314bf229f74dd1af3d1
Shreeya <82967080+ttheshreeyasingh@users.noreply.github.com>
2021-05-08 18:11:55 +0530

    Update queue.c
    isPQueueEmpty and Priority_Enqueue

--- queue.c
+++ queue.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+// PQueue
+typedef stNodepq PQueue;
+typedef stNodepq Nodepq;
+struct stNodepq{
+    element n;
+    key k;  // Lower values indicate higher k
+    Nodepq* pNext;
+ 
+};
 
+// Function to Enqueue according to key 
+void Priority_Enqueue(PQueue** Q, element u, key P)
+{
+    PQueue* start = (*head);
+ 
+    // Create new PQueue
+    PQueue* temp = newNode(u, P);
+ 
+    // Special Case: The head of list has lesser
+    // k than new node. So insert new
+    // node before head node and change head node.
+    if ((*head)->k > P) {
+ 
+        // Insert New PQueue before head
+        temp->pNext = *head;
+        (*head) = temp;
+    }
+    else {
+ 
+        // Traverse the list and find a
+        // position to insert new node
+        while (start->pNext != NULL &&
+            start->pNext->k < P) {
+            start = start->pNext;
+        }
+ 
+        // Either at the ends of the list
+        // or at required position
+        temp->pNext = start->pNext;
+        start->pNext = temp;
+    }
+}
+
+// Function to check is list is empty
+int isPQueueEmpty(PQueue** Q)
+{
+    return (*Q) == NULL;
+}
