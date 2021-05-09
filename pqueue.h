07e7fd417f51ee3011c7fe3c9cf1f2d0d72fcf28
Shreeya <82967080+ttheshreeyasingh@users.noreply.github.com>
2021-05-09 18:50:14 +0530

    Create pqueue.h
    pqueue.h in place of queue.h

---
+++ pqueue.h
@@ -0,0 +1,34 @@
+#ifndef __PQUEUE_H
+#define __PQUEUE_H
+
+#define ll long long int
+
+typedef struct PQueue PQueue; // Priority Queue using Min Heaps
+typedef struct pq_node pq_node; // Node in Priority Queue
+
+typedef long long int Element;
+typedef long long int Key;
+
+
+struct PQueue {
+    pq_node* arr;
+    ll size; // number of elements present
+    ll capacity; // maximum possible number of elements
+};
+
+struct pq_node {
+    Element n;
+    Key k;
+};
+
+PQueue* CreateEmptyPriorityQueue();
+void Priority_Enqueue(PQueue* Q, Element u, Key k);
+int IsPQueueEmpty(PQueue* Q);
+Element ExtractMin(PQueue* Q);
+void DecreaseKey(PQueue* Q, Element u, Key new_decreased_key);
+
+void delete_pqueue(PQueue* Q);
+
+
+
+#endif
